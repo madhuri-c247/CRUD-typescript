b589638719a98f22617a62d97f773e08ee212932
madhuri-c247 <madhuri.v@247madhuri.v@chapter247.com>
2023-08-09 19:12:38 +0530

    fix-edit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,49 +3,42 @@ import Update from "./Update";
 import { Todo } from "./models";
 const Home:React.FC =() =>{
 
-    const [Data, setData] = useState<string>('')
-    const [List, setList]= useState<Todo[]>([])
+    const [data, setData] = useState<string>('')
+    const [list, setList]= useState<Todo[]>([])
     const [idHolder, setidHolder]= useState<number>()
     const [toggle, setToggle]= useState<boolean>(false)
 
-    useEffect(()=>{
-        if(Data){
-            
-        }
-    })
-
     const handleDelete = (id:number)=>{
-        const newlist = List.filter((item)=>item.id != id)
+        const newlist = list.filter((item)=>item.id != id)
         setList(newlist)   
      }
      
      const handleEdit = (id:number)=>{
          console.log(id)
         setidHolder(id)
-        const temp = List.find((item)=>item.id==id)
-        const editing  = temp?.todo
+        const temp = list.find((item)=>item.id==id)
         console.log(temp?.todo)
         setToggle(true)
         
-        // setData(temp?.todo)
+         setData(temp?.todo?temp?.todo:'')
      }
 
      const edit = () =>{
-        const index = List.findIndex((item)=> item.id==idHolder)
+        const index = list.findIndex((item)=> item.id==idHolder)
         const newData = {
             id: Date.now(),
-            todo: Data
+            todo: data
         }
-        List.splice(index, 1, newData)
+        list.splice(index, 1, newData)
         setToggle(false)
         
      }
     return(
         <>
             <h1>Listing</h1>
-            <Update Data={Data} setData={setData} List={List} setList={setList} edit={edit} toggle={toggle} setToggle={setToggle}/>
+            <Update Data={data} setData={setData} List={list} setList={setList} edit={edit} toggle={toggle} setToggle={setToggle}/>
 
-            {List.map((items)=>{
+            {list?list.map((items)=>{
                 return(
                     <div key={items.id}>
                      <label>{items.id}</label>
@@ -54,7 +47,7 @@ const Home:React.FC =() =>{
                      <button onClick={()=>handleEdit(items.id)}>edit</button>
                     </div>
                 )
-            })}  
+            }):'No Data in a List'}  
         </>
     )
 }
